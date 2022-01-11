dfd75715ac23e9328a5ae43fffede6f2530d7bcd
lecarles <35672759+lecarles@users.noreply.github.com>
2022-01-11 14:28:24 +0100

    Utils tweaks

--- pages/web/utils.vue
+++ pages/web/utils.vue
@@ -68,8 +68,8 @@
           <li class="bg-gradient gradient-secondary">
             .gradient-secondary .bg-gradient
           </li>
-          <li class="bg-gradient gradient-feminista">
-            .gradient-feminista .bg-gradient
+          <li class="bg-gradient gradient-feminism">
+            .gradient-feminism .bg-gradient
           </li>
         </b-card-list>
       </b-card>
@@ -81,8 +81,23 @@
           <li class="text-gradient gradient-secondary text-bold">
             .gradient-secondary .text-gradient
           </li>
-          <li class="text-gradient gradient-feminista text-bold">
-            .gradient-feminista .text-gradient
+          <li class="text-gradient gradient-feminism text-bold">
+            .gradient-feminism .text-gradient
+          </li>
+        </b-card-list>
+      </b-card>
+
+      <p class="text-md mt-4">
+        També pots definit un degradat personalitzat amb <code>--gradient-start</code> i <code>--gradient-end</code>
+      </p>
+
+      <b-card type="outline" size="sm" overflow-hidden class="mt-4">
+        <b-card-list>
+          <li style="--gradient-start: var(--indigo); --gradient-end: var(--cyan);" class="bg-gradient">
+            .bg-gradient <code>--gradient-start: var(--indigo)</code> <code>--gradient-end: var(--cyan)</code>
+          </li>
+          <li style="--gradient-start: var(--indigo); --gradient-end: var(--cyan);" class="text-gradient text-bold">
+            .text-gradient <code>--gradient-start: var(--indigo)</code> <code>--gradient-end: var(--cyan)</code>
           </li>
         </b-card-list>
       </b-card>
@@ -90,16 +105,16 @@
       <h3>Tamanys de text</h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>.text-xs</td><td><pre>font-size: 0.75rem</pre></td></tr>
-          <tr><td>.text-sm</td><td><pre>font-size: 0.875rem</pre></td></tr>
-          <tr><td>.text-md</td><td><pre>font-size: 1rem</pre></td></tr>
-          <tr><td>.text-lg</td><td><pre>font-size: 1.125rem</pre></td></tr>
-          <tr><td>.text-xl</td><td><pre>font-size: 1.25rem</pre></td></tr>
-          <tr><td>.text-2xl</td><td><pre>font-size: 1.666rem</pre></td></tr>
-          <tr><td>.text-3xl</td><td><pre>font-size: 2.369rem</pre></td></tr>
-          <tr><td>.text-4xl</td><td><pre>font-size: 3.157rem</pre></td></tr>
-          <tr><td>.text-5xl</td><td><pre>font-size: 4.209rem</pre></td></tr>
-          <tr><td>.text-6xl</td><td><pre>font-size: 5.653rem</pre></td></tr>
+          <tr><td>.text-xs</td><td><code>font-size: 0.75rem</code></td></tr>
+          <tr><td>.text-sm</td><td><code>font-size: 0.875rem</code></td></tr>
+          <tr><td>.text-md</td><td><code>font-size: 1rem</code></td></tr>
+          <tr><td>.text-lg</td><td><code>font-size: 1.125rem</code></td></tr>
+          <tr><td>.text-xl</td><td><code>font-size: 1.25rem</code></td></tr>
+          <tr><td>.text-2xl</td><td><code>font-size: 1.666rem</code></td></tr>
+          <tr><td>.text-3xl</td><td><code>font-size: 2.369rem</code></td></tr>
+          <tr><td>.text-4xl</td><td><code>font-size: 3.157rem</code></td></tr>
+          <tr><td>.text-5xl</td><td><code>font-size: 4.209rem</code></td></tr>
+          <tr><td>.text-6xl</td><td><code>font-size: 5.653rem</code></td></tr>
         </table>
       </b-card>
 
@@ -109,12 +124,12 @@
           <tr>
             <td class="text-bold">
               .text-bold
-            </td><td><pre class="m-0">font-weight: 900</pre></td>
+            </td><td><code class="m-0">font-weight: 900</code></td>
           </tr>
           <tr>
             <td class="text-regular">
               .text-regular
-            </td><td><pre class="m-0">font-weight: 500</pre></td>
+            </td><td><code class="m-0">font-weight: 500</code></td>
           </tr>
         </table>
       </b-card>
@@ -122,32 +137,32 @@
       <h3>Vores</h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>.border</td><td><pre>border: 1px solid --border-color</pre></td></tr>
-          <tr><td>.border-top</td><td><pre>border-top: 1px solid --border-color</pre></td></tr>
-          <tr><td>.border-bottom</td><td><pre>border-bottom: 1px solid --border-color</pre></td></tr>
-          <tr><td>.border-right</td><td><pre>border-right: 1px solid --border-color</pre></td></tr>
-          <tr><td>.border-left</td><td><pre>border-left: 1px solid --border-color</pre></td></tr>
+          <tr><td>.border</td><td><code>border: 1px solid --border-color</code></td></tr>
+          <tr><td>.border-top</td><td><code>border-top: 1px solid --border-color</code></td></tr>
+          <tr><td>.border-bottom</td><td><code>border-bottom: 1px solid --border-color</code></td></tr>
+          <tr><td>.border-right</td><td><code>border-right: 1px solid --border-color</code></td></tr>
+          <tr><td>.border-left</td><td><code>border-left: 1px solid --border-color</code></td></tr>
         </table>
       </b-card>
 
       <h3>Color de les vores</h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>--border-color</td><td>Color de la vora, p.e.: <pre>--border-color: red</pre></td></tr>
-          <tr><td>.border-white</td><td>Fa <pre>--border-color</pre> blanc</td></tr>
-          <tr><td>.border-black</td><td>Fa <pre>--border-color</pre> negre</td></tr>
-          <tr><td>.border-outline</td><td>Fa <pre>--border-color</pre> gris</td></tr>
+          <tr><td>--border-color</td><td>Color de la vora, p.e.: <code>--border-color: red</code></td></tr>
+          <tr><td>.border-white</td><td>Fa <code>--border-color</code> blanc</td></tr>
+          <tr><td>.border-black</td><td>Fa <code>--border-color</code> negre</td></tr>
+          <tr><td>.border-outline</td><td>Fa <code>--border-color</code> gris</td></tr>
         </table>
       </b-card>
 
       <h3>Radi de les vores</h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>.rounded-xs</td><td><pre>border-radius: .5rem</pre></td></tr>
-          <tr><td>.rounded-sm</td><td><pre>border-radius: .75rem</pre></td></tr>
-          <tr><td>.rounded-md</td><td><pre>border-radius: 1rem</pre></td></tr>
-          <tr><td>.rounded-lg</td><td><pre>border-radius: 1.5rem</pre></td></tr>
-          <tr><td>.rounded-xl</td><td><pre>border-radius: 2rem</pre></td></tr>
+          <tr><td>.rounded-xs</td><td><code>border-radius: .5rem</code></td></tr>
+          <tr><td>.rounded-sm</td><td><code>border-radius: .75rem</code></td></tr>
+          <tr><td>.rounded-md</td><td><code>border-radius: 1rem</code></td></tr>
+          <tr><td>.rounded-lg</td><td><code>border-radius: 1.5rem</code></td></tr>
+          <tr><td>.rounded-xl</td><td><code>border-radius: 2rem</code></td></tr>
         </table>
       </b-card>
 
@@ -155,7 +170,7 @@
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
           <tr><td>.shadow</td><td>Afegeix ombra</td></tr>
-          <tr><td>.shadow-rises</td><td>Afegeix ombra en l'estat <pre>:hover</pre></td></tr>
+          <tr><td>.shadow-rises</td><td>Afegeix ombra en l'estat <code>:hover</code></td></tr>
         </table>
       </b-card>
 
@@ -167,51 +182,51 @@
         </table>
       </b-card>
 
-      <h3>Enllaços en estat <pre>:hover</pre></h3>
+      <h3>Enllaços en estat <code>:hover</code></h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>.link-muted-to-black</td><td><a href="" class="link-muted-to-black">Enllaç gris amb <pre>:hover</pre> negre</a></td></tr>
-          <tr><td>.link-muted-to-primary</td><td><a href="" class="link-muted-to-primary">Enllaç gris amb <pre>:hover</pre> taronja</a></td></tr>
-          <tr><td>.link-default-to-primary</td><td><a href="" class="text-color link-default-to-primary">Enllaç color per defecte amb <pre>:hover</pre> taronja</a></td></tr>
-          <tr><td>.link-default-to-gradient</td><td><a href="" class="text-color link-default-to-gradient">Enllaç color per defecte amb <pre>:hover</pre> degradat</a></td></tr>
-          <tr><td>.link-to-underlined</td><td><a href="" class="link-to-underlined">Enllaç amb <pre>:hover</pre> subratllat</a></td></tr>
+          <tr><td>.link-muted-to-black</td><td><a href="" class="link-muted-to-black">Enllaç gris amb <code>:hover</code> negre</a></td></tr>
+          <tr><td>.link-muted-to-primary</td><td><a href="" class="link-muted-to-primary">Enllaç gris amb <code>:hover</code> taronja</a></td></tr>
+          <tr><td>.link-default-to-primary</td><td><a href="" class="text-color link-default-to-primary">Enllaç color per defecte amb <code>:hover</code> taronja</a></td></tr>
+          <tr><td>.link-default-to-gradient</td><td><a href="" class="text-color link-default-to-gradient">Enllaç color per defecte amb <code>:hover</code> degradat</a></td></tr>
+          <tr><td>.link-to-underlined</td><td><a href="" class="link-to-underlined">Enllaç amb <code>:hover</code> subratllat</a></td></tr>
         </table>
       </b-card>
 
-      <h3>Estats <pre>:focus</pre></h3>
+      <h3>Estats <code>:focus</code></h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>.focus-primary</td><td><pre>:focus</pre> taronja</td></tr>
-          <tr><td>.focus-primary-within</td><td><pre>:focus-within</pre> taronja</td></tr>
-          <tr><td>.focus-dark</td><td><pre>:focus</pre> negre</td></tr>
-          <tr><td>.focus-dark-within</td><td><pre>:focus-within</pre> negre</td></tr>
+          <tr><td>.focus-primary</td><td><code>:focus</code> taronja</td></tr>
+          <tr><td>.focus-primary-within</td><td><code>:focus-within</code> taronja</td></tr>
+          <tr><td>.focus-dark</td><td><code>:focus</code> negre</td></tr>
+          <tr><td>.focus-dark-within</td><td><code>:focus-within</code> negre</td></tr>
         </table>
       </b-card>
 
       <h3>Llistes</h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
-          <tr><td>.list-reset</td><td>Elimina les propietats CSS per defecte de <pre>ul</pre></td></tr>
-          <tr><td>.list-horizontal</td><td>Fa les llistes <pre>ul</pre> horitzontals</td></tr>
+          <tr><td>.list-reset</td><td>Elimina les propietats CSS per defecte de <code>ul</code></td></tr>
+          <tr><td>.list-horizontal</td><td>Fa les llistes <code>ul</code> horitzontals</td></tr>
         </table>
       </b-card>
 
-      <h3>Utilitats per a <pre>grid</pre></h3>
+      <h3>Utilitats per a <code>grid</code></h3>
       <b-card type="outline" size="sm" overflow-hidden class="mt-4">
         <table class="table tint-alternate-rows vertical-divisions">
           <tr v-for="n in 4" :key="n">
-            <td>.span-{{ n }}</td><td><pre>grid-column: span {{ n }}</pre></td>
+            <td>.span-{{ n }}</td><td><code>grid-column: span {{ n }}</code></td>
           </tr>
           <tr v-for="n in 4" :key="n">
-            <td>.md-span-{{ n }}</td><td><pre>grid-column: span {{ n }}</pre> (només en dispositius <pre>md</pre>)</td>
+            <td>.md-span-{{ n }}</td><td><code>grid-column: span {{ n }}</code> (només en dispositius <code>md</code>)</td>
           </tr>
           <tr v-for="n in 4" :key="n">
-            <td>.sm-span-{{ n }}</td><td><pre>grid-column: span {{ n }}</pre> (només en dispositius <pre>sm</pre>)</td>
+            <td>.sm-span-{{ n }}</td><td><code>grid-column: span {{ n }}</code> (només en dispositius <code>sm</code>)</td>
           </tr>
         </table>
       </b-card>
 
-      <h3>Utilitats de <pre>bootstrap</pre></h3>
+      <h3>Utilitats de <code>bootstrap</code></h3>
       A banda d'aquestes, el paquet Blobby hereta les <a href="https://getbootstrap.com/docs/5.1/utilities/">utilitats de bootstrap, consultables a la seua documentació</a>.
     </div>
   </div>
@@ -225,17 +240,18 @@ export default {
 </script>
 
 <style lang="scss">
-.page-utils pre {
+.page-utils code {
   margin-bottom: 0;
   display: inline;
   color: inherit;
   border: 1px solid var(--gray-200);
   border-radius: .5rem;
   padding: .2rem;
+  background: transparent;
 }
 
-h3 pre {
-  background: var(--gray-100);
+.page-utils h3 code {
+  background: var(--gray-50);
   border-radius: .5rem;
   padding: .25rem;
 }
